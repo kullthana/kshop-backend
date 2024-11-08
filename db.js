75b354c20c7bd2d6affe06becc578f297c74acb1
kullthana <akoranos@gmail.com>
2024-11-08 23:18:14 +0700

    fix host

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'mysql',
+    host: '127.0.0.1',
+    port: '3306',
     user: 'admin',
     password: 'password',
     database: 'kshop'
